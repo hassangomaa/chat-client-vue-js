aab3adba4e2e2f9cb3212b263b14bd0ec2daa414
hassangomaa <hassangomaa974@gmail.com>
2024-01-10 14:51:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { io } from "socket.io-client";
+import { onBeforeMount, ref } from "vue";
+const socket = io("http://localhost:3001");
+
+const messages = ref([]);
+onBeforeMount(() => {
+  socket.emit("findAllMessages", {}, (response) => {
+    messages.value = response;
+  });
+});
+</script>
+
+<template>
+  <div class="chat">
+    <div class="chat-container">
+      <div class="messages-container">
+        <div v-for="message in messages">
+          [{{ message.name }}] : {{ message.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
